f155c834403ce69abba05b7cc8fc4fd5dd9b8218
acossa01 <adolfocossa@aluno.unilab.edu.br>
2025-10-07 03:26:28 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
-// Esta é a função que Vercel irá chamar.
-// Note que não há 'app.listen()' aqui.
+// Esta é a função que a Vercel irá chamar.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
@@ -15,14 +14,14 @@ async function bootstrap() {
 
   app.use(bodyParser.json({ limit: '500mb' }));
   app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
-  
-  // Prepara a aplicação para receber requisições, mas não a "trava" em uma porta.
+
+  // Prepara a aplicação, mas não a prende a uma porta.
   await app.init(); 
 
-  // Retorna a instância do servidor para a Vercel gerenciar.
+  // Retorna a instância do servidor para a Vercel gerir.
   const expressApp = app.getHttpAdapter().getInstance();
   return expressApp;
 }
 
-// Exportamos a função para a Vercel.
+// Exporta a função para a Vercel usar.
 export default bootstrap();
